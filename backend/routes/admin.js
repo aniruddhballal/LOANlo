ab45f048379f1f1edf76e0421af9f04977135075
ballalthrusts <aniruddhballaldeeksha@gmail.com>
2025-07-25 12:31:51 +0530

    Rolling back to old server.js

--- backend/routes/admin.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const adminController = require('../controllers/adminController');
-const { authenticateToken, requireRole } = require('../middleware/auth');
-
-router.post('/sync-user-data', authenticateToken, requireRole(['admin']), adminController.syncUserData);
-
-module.exports = router;
